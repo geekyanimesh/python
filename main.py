98ec28e8b6d986188bcbe76270ace9231398c9c4
Animesh Kumar <animesh.kumar9651@gmail.com>
2024-11-24 17:54:39 +0530

    Add files via upload

---
+++ main.py
@@ -0,0 +1,40 @@
+# Variables
+
+playerChoice = "rock"       # string
+computerChoice = "paper"
+
+# Functions
+
+# Defning the functions
+def getChoices():
+    playerChoice1 = "rock"      
+    computerChoice1 = "paper"
+
+    return playerChoice1
+
+def greeting():
+    return "Hi"
+
+# Calling the functions
+
+greeting()
+
+# printing it
+
+response = greeting()
+print(response)
+
+# for getChoices():
+
+response1 = getChoices()
+print(response1)
+
+# Dictionaries : Key value pairs in python
+
+dict = {"name":"John", "color":"blue"}
+
+# adding variables to dictionary
+
+dict1 = {"name":"John", "color":response1}
+
+
